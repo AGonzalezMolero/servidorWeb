ff7438456d677e8c697e0b4d26d7562258675a21
Aitor Gonzalez <aitorgonzalezmolero@gmail.com>
2020-05-14 15:23:30 +0200

    cambio contraseña

--- servidor/db.js
+++ servidor/db.js
@@ -20,7 +20,7 @@ class Obj {
         this.port           = 3306
         this.db             = 'serverDB_agonzalez'
         this.usr            = 'agonzalez'
-        this.pwd            = '8ase2pwd'
+        this.pwd            = 'holaamigo'
         this.pool           = null
 
         this.logCyan        = '\x1b[36m%s\x1b[0m '
